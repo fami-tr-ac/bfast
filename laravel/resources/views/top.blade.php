d26e4339a6923113162ee38802022daea4cdd287
fami0719 <71296169+fami0719@users.noreply.github.com>
2022-01-18 21:36:52 +0900

    Update top.blade.php

--- laravel/resources/views/top.blade.php
+++ laravel/resources/views/top.blade.php
@@ -5,6 +5,7 @@
   <div class="container">
     <div class="loginPage_contents">
     <h1 class="h3 loginPage_contents_title">今日の朝ごはんを自慢し合うコミュニティ</h1>
+    <h2 class="h3 loginPage_contents_details">友達や家族に今日の朝ご飯をみてもらいましょう。人にみてもらうことで健康管理ができます。</h2>
     <div class="btn loginPage_contents_btn"><a class="text-white" href="{{ route('login') }}">メールアドレスでログインする</a></div>
     </div>
   </div>
